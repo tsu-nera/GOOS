5b803780b7f59112faa6655080a1e2d49f19791b
tsu-nera <fox10225fox@gmail.com>
2013-02-13 08:17:18 +0900

    p119 test failed

--- test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -1,10 +1,20 @@
 package test.auctionsniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
 import org.junit.Test;
 
+import auctionsniper.AuctionMessageTranslator;
+
 public class AuctionMessageTranslatorTest {
+  public static final Chat UNUSED_CHAT = null;
+  private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
+
 	@Test public void
 	notifiesAuctionClosedWhenCloseMessageRecieved() {
+	  Message message = new Message();
+	  message.setBody("SOLVersion: 1.1; Kvent; CLOSE;");
 
+	  translator.processMessage(UNUSED_CHAT, message);
 	}
 }
