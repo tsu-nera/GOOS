e3d91ee3c4ca4275cef7438873efebe90c1a5ad7
tsu-nera <fox10225fox@gmail.com>
2013-02-12 21:57:39 +0900

    p118

---
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -0,0 +1,10 @@
+package test.auctionsniper;
+
+import org.junit.Test;
+
+public class AuctionMessageTranslatorTest {
+	@Test public void
+	notifiesAuctionClosedWhenCloseMessageRecieved() {
+
+	}
+}
