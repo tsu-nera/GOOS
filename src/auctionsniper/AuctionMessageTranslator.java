57989f56365823f16a017fbb09467a995caccebd
tsu-nera <fox10225fox@gmail.com>
2013-02-26 07:31:04 +0900

    120

---
+++ src/auctionsniper/AuctionMessageTranslator.java
@@ -0,0 +1,13 @@
+package auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator {
+
+  public void processMessage(Chat chat, Message message) {
+    // TODO 自動生成されたメソッド・スタブ
+
+  }
+
+}
--- test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
+++ test/unit/test/auctionsniper/AuctionMessageTranslatorTest.java
@@ -2,16 +2,29 @@ package test.auctionsniper;
 
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import auctionsniper.AuctionEventListener;
 import auctionsniper.AuctionMessageTranslator;
 
+@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
+  private final Mockery context = new Mockery();
+  private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
+
   public static final Chat UNUSED_CHAT = null;
   private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
 
 	@Test public void
 	notifiesAuctionClosedWhenCloseMessageRecieved() {
+	  context.checking(new Expectations() {{
+	    oneOf(listener).auctionClosed();
+	  }});
+
 	  Message message = new Message();
 	  message.setBody("SOLVersion: 1.1; Kvent; CLOSE;");
 
