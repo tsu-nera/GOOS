fe088f21c8c17e0367994d3b7ec860c01bc68b80
tsu-nera <fox10225fox@gmail.com>
2013-03-11 21:51:49 +0900

    p137

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -19,88 +19,65 @@ import org.xmlpull.v1.XmlPullParserException;
 import auctionsniper.ui.MainWindow;
 
 public class Main implements SniperListener {
-	@SuppressWarnings("unused") private Chat notToBeGCd;
+  @SuppressWarnings("unused") private Chat notToBeGCd;
 
-	private static final int ARG_HOSTNAME = 0;
-	private static final int ARG_USERNAME = 1;
-	private static final int ARG_PASSWORD = 2;
-	private static final int ARG_ITEM_ID = 3;
+  private static final int ARG_HOSTNAME = 0;
+  private static final int ARG_USERNAME = 1;
+  private static final int ARG_PASSWORD = 2;
+  private static final int ARG_ITEM_ID = 3;
 
-	public static final String AUCTION_RESOURCE = "Auction";
-	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
-	public static final String AUCTION_ID_FORMAT =
-			ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
+  public static final String AUCTION_RESOURCE = "Auction";
+  public static final String ITEM_ID_AS_LOGIN = "auction-%s";
+  public static final String AUCTION_ID_FORMAT =
+      ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
-	public static final String JOIN_COMMAND_FORMAT = "SOLVersion: 1.1; Command: JOIN;";
-	public static final String BID_COMMAND_FORMAT = "SOLVersion: 1.1; Command: BID; Price: %d;";
+  public static final String JOIN_COMMAND_FORMAT = "SOLVersion: 1.1; Command: JOIN;";
+  public static final String BID_COMMAND_FORMAT = "SOLVersion: 1.1; Command: BID; Price: %d;";
 
-	private MainWindow ui;
+  private MainWindow ui;
 
-	public Main() throws Exception {
-		startUserInterface();
-	}
+  public Main() throws Exception {
+    startUserInterface();
+  }
 
-	public static void main(String... args) throws Exception {
-		Main main = new Main();
-		main.joinAuction(
-				connection(args[ARG_HOSTNAME], args[ARG_USERNAME],
-						   args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
-	}
+  public static void main(String... args) throws Exception {
+    Main main = new Main();
+    main.joinAuction(
+        connection(args[ARG_HOSTNAME], args[ARG_USERNAME],
+            args[ARG_PASSWORD]), args[ARG_ITEM_ID]);
+  }
 
 
-	private void disconnectWhenUICloses(final XMPPConnection connection) {
-		ui.addWindowListener(new WindowAdapter() {
-			@Override public void windowClosed(WindowEvent e) {
-				connection.disconnect();
-			}
-		});
-	}
+  private void disconnectWhenUICloses(final XMPPConnection connection) {
+    ui.addWindowListener(new WindowAdapter() {
+      @Override public void windowClosed(WindowEvent e) {
+        connection.disconnect();
+      }
+    });
+  }
 
-	private static String auctionId(String itemId, XMPPConnection connection) {
-		return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
-	}
+  private static String auctionId(String itemId, XMPPConnection connection) {
+    return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
+  }
 
-	private void startUserInterface() throws Exception {
-	SwingUtilities.invokeAndWait(new Runnable() {
-			public void run() {
-				ui = new MainWindow();
-			}
-		});
-	}
+  private void startUserInterface() throws Exception {
+    SwingUtilities.invokeAndWait(new Runnable() {
+      public void run() {
+        ui = new MainWindow();
+      }
+    });
+  }
 
-	private static XMPPConnection
-	connection(String hostname, String username, String password)
-		throws XMPPException
-	{
-		XMPPConnection connection = new XMPPConnection(hostname);
-		connection.connect();
-		connection.login(username, password, AUCTION_RESOURCE);
-
-		return connection;
-	}
-
-  private void joinAuction(XMPPConnection connection,String itemId)
+  private static XMPPConnection
+  connection(String hostname, String username, String password)
       throws XMPPException
       {
-    disconnectWhenUICloses(connection);
+    XMPPConnection connection = new XMPPConnection(hostname);
+    connection.connect();
+    connection.login(username, password, AUCTION_RESOURCE);
 
-    final Chat chat =
-        connection.getChatManager().createChat(auctionId(itemId, connection), null);
-      this.notToBeGCd = chat;
-
-  Auction auction = new Auction() {
-    public void bid(int amount) {
-      try {
-        chat.sendMessage(String.format(BID_COMMAND_FORMAT, amount));
-      }catch (XMPPException e) {
-        e.printStackTrace();
+    return connection;
       }
-    }
-  };
-  chat.addMessageListener(
-      new AuctionMessageTranslator(new AuctionSniper(auction, this)));
-    chat.sendMessage(JOIN_COMMAND_FORMAT);
-  }
 
   public void sniperLost() {
     SwingUtilities.invokeLater(new Runnable() {
@@ -117,4 +94,47 @@ public class Main implements SniperListener {
       }
     });
   }
+
+
+  private void joinAuction(XMPPConnection connection,String itemId)
+      throws XMPPException
+      {
+    disconnectWhenUICloses(connection);
+
+    final Chat chat =
+        connection.getChatManager().createChat(auctionId(itemId, connection), null);
+    this.notToBeGCd = chat;
+
+    Auction auction = new XMPPAuction(chat);
+    chat.addMessageListener(
+        new AuctionMessageTranslator(new AuctionSniper(auction, this)));
+    chat.sendMessage(JOIN_COMMAND_FORMAT);
+      }
+
+  public class XMPPAuction implements Auction {
+    private final Chat chat;
+
+    public XMPPAuction(Chat chat) {
+      this.chat = chat;
+    }
+
+    public void bid(int amount) {
+      sendMessage(String.format(BID_COMMAND_FORMAT, amount));
+    }
+
+    public void join() {
+      sendMessage(BID_COMMAND_FORMAT);
+    }
+
+    private void sendMessage(final String message) {
+      try {
+        chat.sendMessage(message);
+      }catch (XMPPException e) {
+        e.printStackTrace();
+      }
+    }
+
+
+  }
+
 }
