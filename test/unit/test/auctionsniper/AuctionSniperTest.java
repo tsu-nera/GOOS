c0db1e2d6d43b9daaeed0b0fb3fa25c3b56b788c
tsu-nera <fox10225fox@gmail.com>
2013-03-21 08:03:51 +0900

    p151

--- test/unit/test/auctionsniper/AuctionSniperTest.java
+++ test/unit/test/auctionsniper/AuctionSniperTest.java
@@ -2,6 +2,7 @@ package test.auctionsniper;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +19,9 @@ public class AuctionSniperTest {
   private final Auction auction = context.mock(Auction.class);
 
   private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
-//
+
+  private final States sniperState = context.states("sniper");
+
   @Test public void //p128
   reportsLostWhenAuctionClosed() {
     context.checking(new Expectations() {{
@@ -48,4 +51,46 @@ public class AuctionSniperTest {
 
     sniper.currentPrice(123, 45, PriceSource.FromSniper);
   }
+
+  @Test public void  //p149
+  reportsLostIfAuctionClosesImmediateley() {
+    context.checking(new Expectations() {{
+      atLeast(1).of(sniperListener).sniperLost();
+    }});
+
+    sniper.auctionClosed();
+  }
+
+  @Test public void  //p149
+  reportsWonIfAuctionClosesWhenBidding() {
+    context.checking(new Expectations() {{
+      // auctionを呼び出すが気にしない
+      ignoring(auction);
+      //
+      allowing(sniperListener).sniperBidding();
+      then(sniperState.is("bidding"));
+
+      atLeast(1).of(sniperListener).sniperLost();
+      when(sniperState.is("bidding"));
+    }});
+
+    sniper.currentPrice(123, 45, PriceSource.FromOtherBidder);
+    sniper.auctionClosed();
+  }
+
+  @Test public void //p151
+  reportsWonIfAuctionClosesWhenWinning() {
+    context.checking(new Expectations() {{
+      ignoring(auction);
+      allowing(sniperListener).sniperWinning();
+      then(sniperState.is("winning"));
+
+      atLeast(1).of(sniperListener).sniperWon();
+      when(sniperState.is("winning"));
+    }});
+
+    sniper.currentPrice(123, 45, PriceSource.FromSniper);
+    sniper.auctionClosed();
+  }
+
 }
